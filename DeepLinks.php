2f4dea0366b7801d554eefcc34d6226dad4f6a96
starise <starise@gmail.com>
2015-02-01 04:50:13 +0100

    Adds anchors and id to headers

--- DeepLinks.php
+++ DeepLinks.php
@@ -24,5 +24,30 @@ class DeepLinks
 		add_filter( 'the_content', [ $this, 'deepLinksToContent' ] );
 	}
 
-	public function deepLinksToContent( $content ) { }
+	public function deepLinksToContent( $content )
+	{
+		if ( ! is_single() || is_page() ) {
+			return $content;
+		}
+
+		$pattern = '#(?P<full_tag><(?P<tag_name>h\d)(?P<tag_extra>[^>]*)>(?P<tag_contents>[^<]*)</h\d>)#i';
+		if ( preg_match_all( $pattern, $content, $matches, PREG_SET_ORDER ) ) {
+			$find = [];
+			$replace = [];
+			foreach( $matches as $match ) {
+				if ( strlen( $match['tag_extra'] ) && false !== stripos( $match['tag_extra'], 'id=' ) ) {
+					continue;
+				}
+				$find[]     = $match['full_tag'];
+				$id         = sanitize_title( $match['tag_contents'] );
+				$idAttr     = sprintf( ' id="%s"', $id );
+				$anchorLink = sprintf( '<a class="deep-link" href="#%s"></a>', $id );
+				$anchorLink = apply_filters( 'header_deep_link', $anchorLink, $id );
+				$replace[]  = sprintf( '%1$s<%2$s%3$s%4$s>%5$s</%2$s>', $anchorLink, $match['tag_name'], $match['tag_extra'], $idAttr, $match['tag_contents'] );
+			}
+			$content = str_replace( $find, $replace, $content );
+		}
+
+		return $content;
+	}
 }
